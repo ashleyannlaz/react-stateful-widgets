f652523eab2ce5ff924b99cae1875e24f435158b
ashleyannlaz <paperpastels@gmail.com>
2021-07-12 16:59:07 -0400

    Finished Moods

--- src/components/Input.js
+++ src/components/Input.js
@@ -52,15 +52,14 @@ export default function Input() {
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    textTransform: 'uppercase',
     //color: 'royalblue', /* STEP 2 */
-    color: inputValue.length < 10 ?  'royalblue' : 'crimson',
+    color: inputValue.length > 10 ?  'crimson' : 'royalblue',
   };
 
   return (
     <div className='widget-input container'>
       <h2>Input</h2>
-      <div id='output' style={style}>{inputValue}
+      <div id='output' style={style}>{inputValue.toUpperCase()}
       </div> {/* STEP 3 */}
       <div>
         <input id='input' type='text' onChange={changeInput} value={inputValue} /> {/* STEP 6 */}
